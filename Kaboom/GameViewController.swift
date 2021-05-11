1a84c909d52abcc9225ca788a9d0c261dafae89c
Ignasi Pelayo Justicia <ignasi.pelayo@enti.cat>
2021-05-11 19:39:53 +0200

    Aspect correct

--- Kaboom/GameViewController.swift
+++ Kaboom/GameViewController.swift
@@ -7,28 +7,42 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+
+enum GameConfiguration {
+    enum Core {
+        static let gameWidth: CGFloat = 960
+        static let gameHeight: CGFloat = 540
+    }
+}
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+
+        var factor = view.frame.size.height / GameConfiguration.Core.gameHeight
+
+        if view.frame.size.width / factor < GameConfiguration.Core.gameWidth {
+            factor = view.frame.size.width / GameConfiguration.Core.gameWidth
         }
+
+        let sceneSize = CGSize(width: view.frame.size.width / factor,
+                               height: view.frame.size.height / factor)
+
+        guard let view = self.view as? SKView else { return }
+        // Load the SKScene from 'GameScene.sks'
+        if let scene = SKScene(fileNamed: "GameScene") {
+            // Set the scale mode to scale to fit the window
+            scene.size = sceneSize
+            scene.scaleMode = .aspectFit
+            // Present the scene
+            view.presentScene(scene)
+        }
+
+        view.ignoresSiblingOrder = true
+
+        view.showsFPS = true
+        view.showsNodeCount = true
     }
 
     override var shouldAutorotate: Bool {
